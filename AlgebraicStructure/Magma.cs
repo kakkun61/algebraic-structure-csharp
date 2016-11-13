4757524f102ecedd51454474508e74bb7f46a6e5
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2016-11-14 04:01:14 +0900

    first commit

---
+++ AlgebraicStructure/Magma.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace AlgebraicStructure
+{
+    public interface IMagma<T> where T : IMagma<T>
+    {
+        T op(T e);
+    }
+
+    /// <summary>Associative magma.</summary>
+    interface ISemigroup<T> : IMagma<T> where T : ISemigroup<T>
+    {
+    }
+
+    interface IIdenticalMagma<T> : IMagma<T> where T : IIdenticalMagma<T>
+    {
+    }
+
+    interface IIdenticalMagmaFunctions<T> where T : IIdenticalMagma<T>
+    {
+        ISet<T> Identities { get; }
+    }
+
+    interface ICancellativeMagma<T> : IMagma<T> where T : ICancellativeMagma<T>
+    {
+    }
+
+    /// <summary>Invertible magma. Identical and cancellative.</summary>
+    interface ILoop<T> : ICancellativeMagma<T>, IIdenticalMagma<T> where T : ILoop<T>
+    {
+    }
+
+    interface ILoopFunctions<T> : IIdenticalMagmaFunctions<T> where T : ILoop<T>
+    {
+        ISet<T> Invertibilities { get; }
+    }
+
+    /// <summary>Associative and identical magma.</summary>
+    interface IMonoid<T> : IIdenticalMagma<T>, ISemigroup<T> where T : IMonoid<T>
+    {
+    }
+
+    interface IMonoidFunctions<T> : IIdenticalMagmaFunctions<T> where T : IMonoid<T>
+    {
+    }
+    
+    /// <summary>Associative and identical and cancellative magma.</summary>
+    interface IGroup<T> : ILoop<T>, ISemigroup<T> where T : IGroup<T>
+    {
+    }
+
+    interface IGroupFunctions<T> : ILoopFunctions<T> where T : IGroup<T>
+    {
+    }
+}
