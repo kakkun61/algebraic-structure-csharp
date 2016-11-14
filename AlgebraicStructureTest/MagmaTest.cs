6e1d8d64b646cdb03ca3cb5997d73ddb5763fd6b
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2016-11-14 20:51:20 +0900

    test

---
+++ AlgebraicStructureTest/MagmaTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using AlgebraicStructure.Magma.Implementation;
+
+namespace AlgebraicStructureTest.Magma
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void StringMonoidIdentity()
+        {
+            var es = StringMonoidFunctions.Instance.Identities;
+            var a = new StringMonoid("hi");
+            foreach (var e in es)
+                Assert.AreEqual(e.op(a), a);
+        }
+    }
+}
+
