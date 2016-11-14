ccd139309914fb2d78a2037eed6f5607056c4f23
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2016-11-15 02:34:07 +0900

    associativity

--- AlgebraicStructureTest/MagmaTest.cs
+++ AlgebraicStructureTest/MagmaTest.cs
@@ -8,12 +8,30 @@ namespace AlgebraicStructureTest.Magma
     public class Test
     {
         [Test()]
-        public void StringMonoidIdentity()
+        public void StringMonoidLeftIdentity()
         {
             var es = StringMonoidFunctions.Instance.Identities;
             var a = new StringMonoid("hi");
             foreach (var e in es)
                 Assert.AreEqual(e.op(a), a);
+        }
+
+        [Test()]
+        public void StringMonoidRightIdentity()
+        {
+            var es = StringMonoidFunctions.Instance.Identities;
+            var a = new StringMonoid("hi");
+            foreach (var e in es)
+                Assert.AreEqual(a.op(e), a);
+        }
+
+        [Test()]
+        public void StringMonoidAssociativity()
+        {
+            var a = new StringMonoid("1");
+            var b = new StringMonoid("2");
+            var c = new StringMonoid("3");
+            Assert.AreEqual((a*b)*c, a*(b*c));
         }
     }
 }
